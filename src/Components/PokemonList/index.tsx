c71f6abfe8c0cfe25a7427f9776f7325c04a00bf
4lPeReZ <alvaropr05@hotmail.com>
2023-10-01 12:30:15 +0200

    Pokermon List and Card

---
+++ src/Components/PokemonList/index.tsx
@@ -0,0 +1,19 @@
+import { PokemonCard } from "../PokemonCard";
+
+import styles from "./styles.module.scss";
+
+interface Props {
+  pokemonsUrls?: string[] | null;
+  page?: number;
+  perPage?: number;
+}
+
+export const PokemonList = ({ pokemonsUrls, page, perPage }: Props) => {
+  return (
+    <div>
+      {pokemonsUrls?.map((pokemonUrl) => (
+          <PokemonCard key={pokemonUrl} url={pokemonUrl} />
+        ))}
+    </div>
+  );
+};
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import { PokeballIconSmall } from "../../Assets/pokeball";
+import { PokemonList } from "../../Components/PokemonList";
 import styles from "./styles.module.scss";
+import { PokemonContext } from "../../Context/pokemonContext";
 
 export const Home = () => {
+  const { pokemonsFiltered } = useContext(PokemonContext);
 
   return (
     <div className={styles.home}>
@@ -11,6 +15,7 @@ export const Home = () => {
                 <span>Pokedex</span>
             </div>
         </header>
+        <PokemonList pokemonsUrls={pokemonsFiltered} />
     </div>
   );
 };
